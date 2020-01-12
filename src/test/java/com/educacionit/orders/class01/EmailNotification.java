9d441dc98adb83c2bb6a49c629144836c50a710e
Leonel Chaves <leonel.a.cha@gmail.com>
2020-01-12 17:19:59 -0300

    Corrección

--- src/test/java/com/educacionit/orders/class01/EmailNotification.java
+++ src/test/java/com/educacionit/orders/class01/EmailNotification.java
@@ -2,6 +2,6 @@ package com.educacionit.orders.class01;
 
 public class EmailNotification implements INotification {
     public void send(String to, String from, String subject, String content) {
-        System.out.println (String.format ("%s sending message [%s][%s] to %s using email !!!", from, subject, content, to));
+        System.out.println(String.format("%s sending message [%s][%s] to %s using email !!!", from, subject, content, to));
     }
 }
--- src/test/java/com/educacionit/orders/class01/PushNotification.java
+++ src/test/java/com/educacionit/orders/class01/PushNotification.java
@@ -2,6 +2,6 @@ package com.educacionit.orders.class01;
 
 public class PushNotification implements INotification {
     public void send(String to, String from, String subject, String content) {
-        System.out.println (String.format ("%s sending message [%s][%s] to %s using PUSH !!!", from, subject, content, to));
+        System.out.println(String.format("%s sending message [%s][%s] to %s using PUSH !!!", from, subject, content, to));
     }
 }
